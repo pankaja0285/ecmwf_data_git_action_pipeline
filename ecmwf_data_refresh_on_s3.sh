c0225c36de3161375112e18154f9f69cca08866d
Pankaja Shankar <pankaja_us@yahoo.com>
2025-09-11 23:12:29 -0400

    minor typo

--- ecmwf_data_refresh_on_s3.sh
+++ ecmwf_data_refresh_on_s3.sh
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 
 # ecmwf data processing main kick-off
 # local
-python main_ecmwf_data_pipeline.py --download_path="download" --prepped_paths="prepped" \
+python main_ecmwf_data_pipeline.py --download_path="download" --prepped_path="prepped" \
 --prepped_suffix="temp" \
 --filter_levels="surface, heightAboveGround" --number_of_days='2' --step_counter=6 \
 --push_destination="s3" --push_data_path="data" --env="env" --delete_s3_files_flag="Y"
